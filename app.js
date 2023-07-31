3aa7afbdd6d4293fc3ff4909622709d394afbd6a
VratikaBhardwaj <111068952+VratikaBhardwaj@users.noreply.github.com>
2023-07-31 22:06:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <>
+      
+      <div className="container">
+        <TextForm heading="Enter the text to analyze"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
